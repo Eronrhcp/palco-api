3c663a510fd19ca3a44cf6ccbebae6bdf472250e
Eron Oliveira <eron.fisica@gmail.com>
2026-08-20 18:27:03 -0300

    feat: add Spring Data JPA repositories for all entities

---
+++ src/main/java/com/palco/api/repository/CifraRepository.java
@@ -0,0 +1,7 @@
+package com.palco.api.repository;
+
+import com.palco.api.model.Cifra;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CifraRepository extends JpaRepository<Cifra, Long> {
+}
